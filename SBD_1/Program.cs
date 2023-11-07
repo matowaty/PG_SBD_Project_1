efd5f1e1407e45d12211d7f36a928a556116f6c1
matowaty <matinowak24@gmail.com>
2023-11-07 16:51:40 +0100

    Dodaj pliki projektów.

---
+++ SBD_1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace SBD_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fileHandler file = new fileHandler();
+            Console.WriteLine("Hello World!");
+
+            //file.writeBlock();
+            Node value = file.readRecord(1);
+
+            file.writeBlock();
+
+            
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public struct Node
+    {
+        public int voltage;
+        public int current;
+        public int resistance;
+        public Node(int voltage, int current, int resistance)
+        {
+            this.voltage = voltage;
+            this.current = current;
+            this.resistance = resistance;
+        }
+    }
+}
---
+++ SBD_1/fileHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_1
+{
+    class fileHandler
+    {
+        private const int structSize = 3;
+        private const int blockSize = structSize * 1;
+        private const string file = "../../../test.bin";
+        private const int intSize = 4;
+        private int[] blockInMemory = new int[blockSize];
+        private int blockInMemoryIndex = -1;
+
+        public fileHandler()
+        {
+            Console.WriteLine("new handler created");
+        }
+
+        public void writeBlock(int blockIndex=1){
+            Console.WriteLine("writing");
+            //block in memory -> temp byte array -> save to pointer in file
+            byte[] buffer = new byte[blockSize*intSize];
+            byte[] tmp = new byte[intSize];
+            Console.WriteLine(blockInMemory[0].GetType().Name);
+            for(int i = 0; i < blockSize; i++)
+            {
+                tmp = BitConverter.GetBytes(blockInMemory[i]);
+                buffer[i*intSize] = tmp[0];
+                buffer[i*intSize +1] = tmp[1];
+                buffer[i*intSize +2] = tmp[2];
+                buffer[i*intSize +3] = tmp[3];
+            }
+            Console.WriteLine(buffer);
+            buffer[1] = 0;
+            buffer[3] = 0;
+            Console.WriteLine(buffer);
+            using (BinaryWriter writer = new BinaryWriter(new FileStream(file, FileMode.Open)))
+            {
+                writer.BaseStream.Seek(blockIndex*intSize, SeekOrigin.Begin);
+                writer.Write(buffer, 0, blockSize * intSize);
+            }
+
+
+        }
+        private void readBlock(int blockIndex=0){
+            byte[] buffer = new byte[blockSize * intSize];
+            using (BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open)))
+            {
+                reader.BaseStream.Seek(blockIndex, SeekOrigin.Begin);
+                reader.Read(buffer, 0, blockSize*intSize);
+            }
+
+            for(int i=0; i<blockSize;i++)
+            {
+                blockInMemory[i]=BitConverter.ToInt32(buffer, i*intSize);
+            }
+            blockInMemoryIndex = blockIndex;
+        }
+
+        public Node readRecord(int index)
+        {
+            int blockIndex = index / blockSize;
+            if(blockInMemoryIndex!=blockIndex)
+            {
+                readBlock(blockIndex);
+            }
+            int internalIndex = index % blockSize;
+
+            Node readValue = new Node(blockInMemory[internalIndex*structSize], blockInMemory[internalIndex*structSize+1], blockInMemory[internalIndex*structSize+2]);
+
+            return readValue;
+
+        }
+    }
+}
